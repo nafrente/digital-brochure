d2ad4ff4a998247de6928704f880d78bda4671e1
Herton <herton@mentel.com>
2016-12-26 11:02:19 -0500

    First version of fastlease digital brochure

---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>FastLease Digital Brochure</title>
+<!-- Meta-->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<!-- Font -->
+<link href="https://fonts.googleapis.com/css?family=Arimo:400,700" rel="stylesheet">
+<!-- CSS -->
+
+<!-- Base style -->
+<link type="text/css" rel="stylesheet" href="assets/digital-brochure.css">
+<link rel="stylesheet" href="assets/font-awesome.min.css">
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery.js"></script>
+
+
+</head>
+<!-- begin body output -->
+<body>
+    <div class="digital-brochure">
+        <header>
+            <div class="db-logo">
+                <img src="assets/A_Plus_Logo.png" alt="Real Estate Brochure logo" />
+            </div>
+            <div class="db-agent">
+                <div class="img-content">
+                    <img src="assets/agent.png" alt="Map placeholder" />
+                </div>
+                <div class="txt-content">
+                    <div class="titles">
+                        <h1>Natalie Young</h1><br>
+                        <h4>Licensed Real Estate Agent</h4>
+                    </div>
+                    <div class="clear"></div>
+                    <p>512-XXX-XXXX</p>
+                    <p>natalie@apluapartments.com</p>
+                </div>
+
+            </div>
+        </header>
+        
+        <section class="db-map">
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+
+        </section>
+
+        <section class="db-properties">
+
+            <article class="db-property">
+                <a href="internal.php" class="property-link">
+                    <div class="top-section">
+                        <h2>Property Name</h2>
+                        <span class="db-star">
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="img-section">
+                        <img src="assets/property.jpeg" alt="Map placeholder" />
+                    </div>
+                    <div class="desc-section">
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                    </div>
+                </a>
+            </article>
+            
+            <article class="db-property">
+                <a href="internal.php" class="property-link">
+                    <div class="top-section">
+                        <h2>Property Name</h2>
+                        <span class="db-star">
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="img-section">
+                        <img src="assets/property.jpeg" alt="Map placeholder" />
+                    </div>
+                    <div class="desc-section">
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                    </div>
+                </a>
+            </article>
+            
+            <article class="db-property">
+                <a href="internal.php" class="property-link">
+                    <div class="top-section">
+                        <h2>Property Name</h2>
+                        <span class="db-star">
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="img-section">
+                        <img src="assets/property.jpeg" alt="Map placeholder" />
+                    </div>
+                    <div class="desc-section">
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                    </div>
+                </a>
+            </article>
+
+            <article class="db-property">
+                <a href="internal.php" class="property-link">
+                    <div class="top-section">
+                        <h2>Property Name</h2>
+                        <span class="db-star">
+                            <i class="fa fa-star" aria-hidden="true"></i>
+                        </span>
+                    </div>
+
+                    <div class="img-section">
+                        <img src="assets/property.jpeg" alt="Map placeholder" />
+                    </div>
+                    <div class="desc-section">
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                        <div class="line">
+                            <span>Beds/Baths</span> | <span>Sqft</span> | <span>Low rent - High Rent</span>
+                        </div>
+                    </div>
+                </a>
+            </article>
+
+        </section>
+
+    </div>
+
+
+
+<!-- JAVASCRIPT CONTROLS -->
+<script type="text/javascript">
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+</script>
+</body>
+<!-- end body output -->
+</html>
